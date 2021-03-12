4b2cddecada892ecd08f963eee1d6423489108ce
GUMBAUElric <elric.gumbau@ynov.com>
2021-03-12 23:59:27 +0100

    Update route

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Restaurant.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '*',
-    redirect: '/home',
+    redirect: '/restaurants',
   },
   {
-    path: '/home',
+    path: '/restaurants',
     name: 'Home',
     component: Home,
   },
