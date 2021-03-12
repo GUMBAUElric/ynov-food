db5fd195e9f6c0d92485dddfa1645ee5f6acefcb
GUMBAUElric <elric.gumbau@ynov.com>
2021-03-12 18:32:06 +0100

    Create component search bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="container"></div>
+  <div class="container__home">
+    <yfSearchBar />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import yfSearchBar from '@/components/home/yfSearchBar.vue'
 
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    yfSearchBar,
+  },
 }
 </script>
 
 <style scoped>
-.container {
-  height: 90vh;
+.container__home {
+  width: 100%;
+  height: 85vh;
 }
 </style>
