6db1d4429cdc9edf6dc3839df593fa73e41c8754
GUMBAUElric <elric.gumbau@ynov.com>
2021-03-12 19:09:39 +0100

    Create card category

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="container__home">
     <yfSearchBar />
+    <div class="container__cards__category">
+      <yfCardCategory v-for="(item, idx) in 8" :key="idx" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import yfSearchBar from '@/components/home/yfSearchBar.vue'
+import yfCardCategory from '@/components/home/yfCardCategory.vue'
 
 export default {
   name: 'Home',
   components: {
     yfSearchBar,
+    yfCardCategory,
   },
 }
 </script>
@@ -20,5 +25,14 @@ export default {
 .container__home {
   width: 100%;
   height: 85vh;
+}
+
+.container__home .container__cards__category {
+  width: 100%;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
 }
 </style>
