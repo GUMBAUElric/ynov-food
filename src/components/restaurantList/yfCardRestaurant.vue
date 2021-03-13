e60a07da8e195a890cc12ac1dd98b856240ce5c5
GUMBAUElric <elric.gumbau@ynov.com>
2021-03-13 22:35:58 +0100

    Update card restaurant

---
+++ src/components/restaurantList/yfCardRestaurant.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="card__restaurant">
+    <div class="container__card__restaurant">
+      <div class="image_restaurant">
+        <img
+          src="https://s3-media3.fl.yelpcdn.com/bphoto/LiSESAyRJzf9m3CWPpeNpg/o.jpg"
+          alt="image_restaurant"
+        />
+      </div>
+      <div class="name_restaurant">
+        <h3>La casa de papel</h3>
+      </div>
+      <div class="categorie_restaurant">
+        <p>Italian</p>
+      </div>
+      <div class="rating_restaurant">
+        <i class="fas fa-star" v-for="(item, idx) in 5" :key="idx"></i>
+      </div>
+      <div class="city_restaurant">
+        <i class="fas fa-map-marker-alt"></i>
+        <p>Lyon</p>
+      </div>
+      <div class="see_details">
+        <button>
+          Voir en détails
+          <i class="fas fa-chevron-right"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'yfCardRestaurant',
+}
+</script>
+
+<style scoped>
+.card__restaurant {
+  width: 180px;
+  margin: 40px 50px;
+  border-radius: 20px;
+  background-color: #fff;
+  transition: 300ms ease;
+  z-index: 0;
+}
+
+.card__restaurant .container__card__restaurant {
+  margin: 20px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.card__restaurant .container__card__restaurant .image_restaurant img {
+  width: 70px;
+  height: 70px;
+  margin-top: -50px;
+  border-radius: 50%;
+}
+
+.card__restaurant .container__card__restaurant .name_restaurant {
+  margin: 20px 0;
+}
+
+.card__restaurant .container__card__restaurant .rating_restaurant {
+  margin: 20px 0;
+}
+
+.card__restaurant .container__card__restaurant .rating_restaurant i {
+  margin: 0 5px;
+}
+
+.card__restaurant .container__card__restaurant .city_restaurant {
+  display: flex;
+  align-items: center;
+}
+
+.card__restaurant .container__card__restaurant .city_restaurant p {
+  margin-left: 5px;
+}
+
+.card__restaurant .container__card__restaurant .see_details {
+  margin-top: 10px;
+}
+
+.card__restaurant .container__card__restaurant .see_details button {
+  position: relative;
+  height: 40px;
+  width: 130px;
+  margin-top: 15px;
+  border-radius: 20px;
+  background-color: #fff;
+  z-index: 0;
+  transition: 300ms;
+  cursor: pointer;
+  overflow: hidden;
+  border: 3px solid var(--primary-color);
+}
+
+.card__restaurant .container__card__restaurant .see_details button:hover {
+  width: 160px;
+}
+
+.card__restaurant .container__card__restaurant .see_details button::before {
+  content: '';
+  position: absolute;
+  height: 40px;
+  width: 160px;
+  border-radius: 20px;
+  top: -2px;
+  left: -2px;
+  transition: 300ms;
+  z-index: -1;
+  transform: translate(-100%);
+  background-color: var(--primary-color);
+}
+
+.card__restaurant .container__card__restaurant .see_details button:hover::before {
+  transform: translate(0%);
+}
+
+.card__restaurant .container__card__restaurant .see_details button i {
+  transform: scale(0);
+  margin-left: -10px;
+  font-size: 0.9em;
+  transition: 300ms;
+}
+
+.card__restaurant .container__card__restaurant .see_details button:hover i {
+  display: inline;
+  margin-left: 10px;
+  transform: scale(1);
+}
+</style>
---
+++ src/components/restaurantList/yfSearchBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="search_bar">
+    <input type="text" placeholder="Search food, restaurant, ..." />
+    <i class="fas fa-search"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'yfSearchBar',
+}
+</script>
+
+<style scoped>
+.search_bar {
+  height: 40px;
+  border-radius: 20px;
+  margin-top: 40px;
+  display: flex;
+  align-items: center;
+  background-color: #faf9fb;
+}
+
+.search_bar input {
+  height: 100%;
+  width: 160px;
+  margin: 10px 10px;
+  border-color: transparent;
+  background-color: transparent;
+  color: var(--text-color);
+}
+
+.search_bar i {
+  margin-right: 20px;
+  margin-left: 10px;
+  color: var(--text-color);
+}
+</style>
