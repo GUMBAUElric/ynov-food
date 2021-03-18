678ebf0b0dbea562cba0b421a8cd3c70e9cc0faf
GUMBAUElric <elric.gumbau@ynov.com>
2021-03-18 19:12:21 +0100

    Update filter

---
+++ src/components/restauranstList/yfRestaurantsListFilter.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="filter">
+    <div class="container__filter">
+      <div class="aroud-me">
+        <input type="checkbox" id="aroundMe" name="aroundMe" />
+        <label for="aroundMe">Autour de moi</label>
+      </div>
+      <div class="is-open">
+        <input type="checkbox" id="isOpen" name="isOpen" />
+        <label for="isOpen">Restaurants ouvert</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'yfRestaurantsListFilter',
+}
+</script>
+
+<style scoped>
+.filter {
+  height: 40px;
+  border-radius: 20px;
+  margin-top: 40px;
+  display: flex;
+  align-items: center;
+  color: #faf9fb;
+}
+
+.filter .container__filter {
+  margin: 15px 20px;
+  display: flex;
+  flex-direction: column;
+}
+
+.filter .container__filter .is-open {
+  margin-top: 10px;
+}
+</style>
