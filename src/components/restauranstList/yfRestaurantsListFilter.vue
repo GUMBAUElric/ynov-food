99126cc6d516ee5966352a9c30b0ff6eec636d60
GUMBAUElric <elric.gumbau@ynov.com>
2021-03-18 23:56:28 +0100

    Filter by geolocation and opening

--- src/components/restauranstList/yfRestaurantsListFilter.vue
+++ src/components/restauranstList/yfRestaurantsListFilter.vue
@@ -2,11 +2,23 @@
   <div class="filter">
     <div class="container__filter">
       <div class="aroud-me">
-        <input type="checkbox" id="aroundMe" name="aroundMe" />
+        <input
+          type="checkbox"
+          id="aroundMe"
+          name="aroundMe"
+          v-model="filter.aroudMe"
+          @change="filterByGeoLocation"
+        />
         <label for="aroundMe">Autour de moi</label>
       </div>
       <div class="is-open">
-        <input type="checkbox" id="isOpen" name="isOpen" />
+        <input
+          type="checkbox"
+          id="isOpen"
+          name="isOpen"
+          v-model="filter.isOpen"
+          @change="filterByOpening"
+        />
         <label for="isOpen">Restaurants ouvert</label>
       </div>
     </div>
@@ -14,8 +26,43 @@
 </template>
 
 <script>
+/** Import */
+import { mapActions } from 'vuex'
+import Geolocation from '@/assets/modules/Geolocation'
+
+const { getPermissionStatus, getGeolocation } = Geolocation()
+
 export default {
   name: 'yfRestaurantsListFilter',
+  data() {
+    return {
+      filter: {
+        aroudMe: false,
+        isOpen: false,
+      },
+    }
+  },
+  methods: {
+    ...mapActions(['fetchRestaurants']),
+    async filterByGeoLocation() {
+      if (this.filter.aroudMe) {
+        try {
+          const permission = await getPermissionStatus()
+
+          if (permission === 'granted') {
+            const { latitude, longitude } = await getGeolocation()
+            this.fetchRestaurants({ latitude, longitude, radius: '40000' })
+          }
+        } catch (error) {
+          console.error(error)
+        }
+      } else this.fetchRestaurants()
+    },
+    filterByOpening() {
+      if (this.filter.isOpen) this.fetchRestaurants({ open_now: true })
+      else this.fetchRestaurants()
+    },
+  },
 }
 </script>
 
