26a56291d0b81ea75830b560abee36f358034fc9
GUMBAUElric <elric.gumbau@ynov.com>
2021-03-18 23:55:48 +0100

    Create component slider

--- src/components/restauranstList/yfRestaurantsListSlider.vue
+++ src/components/restauranstList/yfRestaurantsListSlider.vue
@@ -1,11 +1,46 @@
 <template>
-  <div>oui</div>
+  <div class="range">
+    <div class="field">
+      <div class="value">{{ range }}</div>
+      <input type="range" min="0" max="5" v-model="range" />
+      <i class="fas fa-star"></i>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'yfRestaurantsListSlider',
+  data() {
+    return {
+      range: 0,
+    }
+  },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.range {
+  display: flex;
+  margin-top: 40px;
+  align-items: center;
+  justify-content: center;
+}
+
+.range .field {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+}
+
+.range .field input {
+  margin: 0 20px;
+  height: 3px;
+  width: 100%;
+  border-radius: 5px;
+  outline: none;
+  border: none;
+  background-color: #ddd;
+}
+</style>
