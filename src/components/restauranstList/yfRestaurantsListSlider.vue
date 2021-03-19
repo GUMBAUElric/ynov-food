1fca123b3806f75166edde2a20b2d1ab37a83855
GUMBAUElric <elric.gumbau@ynov.com>
2021-03-19 16:14:53 +0100

    Update component slider

--- src/components/restauranstList/yfRestaurantsListSlider.vue
+++ src/components/restauranstList/yfRestaurantsListSlider.vue
@@ -2,19 +2,37 @@
   <div class="range">
     <div class="field">
       <div class="value">{{ range }}</div>
-      <input type="range" min="0" max="5" v-model="range" />
+      <input type="range" min="0" max="5" step="0.5" value="0" @change="filterByRating($event)" />
       <i class="fas fa-star"></i>
     </div>
   </div>
 </template>
 
 <script>
+/** Import */
+import { mapActions } from 'vuex'
+
 export default {
   name: 'yfRestaurantsListSlider',
   data() {
     return {
-      range: 0,
+      range: '0',
     }
+  },
+  methods: {
+    ...mapActions(['fetchRestaurants', 'updateRating']),
+    /**
+     * @function filterByRating
+     * @desc Ths method filter restaurant by rating
+     * @param {event} e Change evenement
+     * @returns {void}
+     */
+    async filterByRating(e) {
+      const { value } = e.target
+      this.range = value
+      this.updateRating(value)
+      this.fetchRestaurants()
+    },
   },
 }
 </script>
