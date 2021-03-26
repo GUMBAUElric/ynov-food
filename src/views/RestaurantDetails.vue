c20e5d0861dcf7b747c8d58a87ee9690fba654b3
GUMBAUElric <elric.gumbau@ynov.com>
2021-03-26 19:47:06 +0100

    Update view restaurant details

--- src/views/RestaurantDetails.vue
+++ src/views/RestaurantDetails.vue
@@ -1,52 +1,66 @@
 <template>
-  <div class="d-flex restaurant-details">
-    <div class="left justify-content-center">
-      oui
+  <div class="d-flex justify-content-center align-items-center restaurant-details">
+    <div class="d-flex flex-column justify-content-center container" v-if="display">
+      <div class="top">
+        <yfRestaurantDetailsInfos />
+      </div>
+      <div class="middle"></div>
+      <div class="bottom"></div>
     </div>
-    <div class="right justify-content-center align-items-center">
-      <yfRestaurantDetailsCardStep />
-    </div>
+    <div v-else>Veuillez attendre svp</div>
   </div>
 </template>
 
 <script>
 /** Import */
-import { mapActions, mapState } from 'vuex'
-import yfRestaurantDetailsCardStep from '@/components/restaurantDetails/yfRestaurantDetailsCardStep.vue'
+import { mapActions } from 'vuex'
+import yfRestaurantDetailsInfos from '@/components/restaurantDetails/yfRestaurantDetailsInfos.vue'
 
 export default {
   name: 'RestaurantDetails',
   components: {
-    yfRestaurantDetailsCardStep,
+    yfRestaurantDetailsInfos,
   },
   data() {
     return {
       id_restaurant: this.$route.params.id,
+      display: false,
     }
   },
-  computed: {
-    ...mapState(['restaurant_details']),
-  },
   methods: {
-    ...mapActions(['fetchRestaurantsDetails']),
+    ...mapActions(['fetchRestaurantsDetails', 'updateRestaurantDetails']),
   },
-  created() {
-    this.fetchRestaurantsDetails(this.id_restaurant)
+  async created() {
+    try {
+      await this.fetchRestaurantsDetails(this.id_restaurant)
+      this.display = true
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  beforeDestroy() {
+    this.updateRestaurantDetails({})
   },
 }
 </script>
 
 <style scoped>
 .restaurant-details {
-  height: 90vh;
+  width: 100%;
 }
 
-.restaurant-details .left {
-  width: 65%;
+.restaurant-details .container {
+  width: 80%;
 }
 
-.restaurant-details .right {
-  width: 35%;
-  height: 100%;
+.restaurant-details .container .top {
+  width: 100%;
+  margin: 40px 0;
+}
+
+@media screen and (min-width: 1600px) {
+  .restaurant-details .container {
+    width: 1700px;
+  }
 }
 </style>
