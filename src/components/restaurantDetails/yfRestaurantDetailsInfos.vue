8b95264cf4dec5f98a1aead768614fe7f39c4049
GUMBAUElric <elric.gumbau@ynov.com>
2021-03-27 01:52:40 +0100

    Update restaurant details infos

--- src/components/restaurantDetails/yfRestaurantDetailsInfos.vue
+++ src/components/restaurantDetails/yfRestaurantDetailsInfos.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="d-flex flex-column align-items-center restaurant-infos">
-    <div class="restaurant-title">
+    <div class="d-flex flex-wrap align-items-center restaurant-title">
       <h1>{{ restaurant_details.name }}</h1>
+      <i v-if="isFavorite" class="fas fa-heart"></i>
     </div>
     <div class="d-flex flex-column justify-content-center align-items-center container">
       <div class="title">
@@ -43,7 +44,7 @@ import { mapState } from 'vuex'
 export default {
   name: 'yfRestaurantDetailsInfos',
   computed: {
-    ...mapState(['restaurant_details']),
+    ...mapState(['restaurant_details', 'favorites']),
     /**
      * @computed fetchCategories
      * @desc Join categories by ', '
@@ -53,12 +54,20 @@ export default {
       return this.restaurant_details.categories.map(item => item.title).join(', ')
     },
     /**
-     * @computed fetchCategories
+     * @computed fetchLocation
      * @desc Join location by ', '
      * @returns {string}
      */
     fetchLocation() {
       return this.restaurant_details.location.display_address.map(item => item).join(', ')
+    },
+    /**
+     * @computed isFavorite
+     * @desc Return if restaurant is favorite
+     * @returns {boolean}
+     */
+    isFavorite() {
+      return this.favorites.find(item => item['.key'] === this.restaurant_details.id) !== undefined
     },
   },
 }
@@ -72,6 +81,10 @@ export default {
 .restaurant-infos .restaurant-title h1 {
   font-size: 3.3em;
   font-weight: 300;
+}
+
+.restaurant-infos .restaurant-title i {
+  margin-left: 10px;
 }
 
 .restaurant-infos .container {
