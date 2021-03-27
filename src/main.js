2fe2d861b8497f58c8083557f7ed6d805eca46bf
GUMBAUElric <elric.gumbau@ynov.com>
2021-03-27 01:51:40 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
+/* eslint-disable global-require */
 import Vue from 'vue'
 import vSelect from 'vue-select'
+import { Icon } from 'leaflet'
 import { Notyf } from 'notyf'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'notyf/notyf.min.css'
 import 'vue-select/dist/vue-select.css'
-import 'flatpickr/dist/flatpickr.min.css'
+import 'leaflet/dist/leaflet.css'
+
+delete Icon.Default.prototype._getIconUrl
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+})
 
 Vue.component('v-select', vSelect)
 
