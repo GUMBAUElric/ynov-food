e014bd125373ecfef2e90ace82c79ab4c49468b5
GUMBAUElric <elric.gumbau@ynov.com>
2021-04-01 00:26:51 +0200

    Update component booking

--- src/components/restaurantDetails/yfRestaurantDetailsBooking.vue
+++ src/components/restaurantDetails/yfRestaurantDetailsBooking.vue
@@ -8,7 +8,7 @@
           </div>
           <div class="d-flex justify-content-around step">
             <div class="d-flex align-items-center step-booking">
-              <div class="icon active">
+              <div class="icon" :class="!isStepPayment ? 'active' : ''">
                 <i class="far fa-calendar-alt"></i>
               </div>
               <div class="infos">
@@ -17,7 +17,7 @@
               </div>
             </div>
             <div class="d-flex align-items-center step step-payment">
-              <div class="icon">
+              <div class="icon" :class="isStepPayment ? 'active' : ''">
                 <i class="far fa-shopping-cart"></i>
               </div>
               <div class="infos">
@@ -29,58 +29,8 @@
         </div>
       </div>
       <div class="d-flex flex-column justify-content-around align-items-center card-material-body">
-        <div class="top">
-          <div class="d-flex flex-column align-items-center container-top">
-            <div class="d-flex content-form">
-              <div class="d-flex flex-column form">
-                <h3>Nom</h3>
-                <input type="text" v-model="booking.name" />
-              </div>
-              <div class="d-flex flex-column form">
-                <h3>E-mail</h3>
-                <input type="email" v-model="booking.email" />
-              </div>
-            </div>
-            <div class="d-flex content-form">
-              <div class="d-flex flex-column form">
-                <h3>Nombre de personne</h3>
-                <select v-model="booking.nb_of_persons">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                  <option>6</option>
-                </select>
-              </div>
-              <div class="d-flex flex-column form">
-                <h3>Heure de réservation</h3>
-                <select v-model="booking.reservation.time">
-                  <option>20:30</option>
-                  <option>21:00</option>
-                  <option>21:30</option>
-                  <option>22:00</option>
-                  <option>22:30</option>
-                </select>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="middle">
-          <div class="d-flex align-items-center content-form">
-            <div class="d-flex flex-column form">
-              <h3>Jour de réservation</h3>
-              <yfDatePicker @dateSelected="handleReservationDay" />
-            </div>
-            <div class="d-flex flex-column form">
-              <h3>Message (falcutatif)</h3>
-              <textarea cols="24" rows="6" v-model="booking.message"></textarea>
-            </div>
-          </div>
-        </div>
-        <div class="bottom">
-          <button class="btn btn-secondary" @click="goToStepPayment">Suivant</button>
-        </div>
+        <yfRestaurantDetailsBookingInfos @bookingData="goToStepPayment" />
+        <yfRestaurantDetailsBookingPayment />
       </div>
     </div>
   </div>
@@ -89,27 +39,21 @@
 <script>
 /** Import */
 import { mapActions, mapState } from 'vuex'
-import yfDatePicker from '@/components/yfDatePicker.vue'
+import yfRestaurantDetailsBookingInfos from '@/components/restaurantDetails/yfRestaurantDetailsBookingInfos.vue'
+import yfRestaurantDetailsBookingPayment from '@/components/restaurantDetails/yfRestaurantDetailsBookingPayment.vue'
+import Payment from '@/assets/utils/Payment'
 
 export default {
   name: 'yfRestaurantDetailsBooking',
   inject: ['notyf'],
   components: {
-    yfDatePicker,
+    yfRestaurantDetailsBookingInfos,
+    yfRestaurantDetailsBookingPayment,
   },
   data() {
     return {
-      booking: {
-        name: '',
-        email: '',
-        nb_of_persons: '1',
-        reservation: {
-          day: '',
-          time: '20:30',
-        },
-        message: '',
-        is_checkout: false,
-      },
+      isStepPayment: false,
+      payment: Payment(),
     }
   },
   computed: {
@@ -117,47 +61,17 @@ export default {
   },
   methods: {
     ...mapActions(['addToBookings']),
-    handleReservationDay(day) {
-      this.booking.reservation.day = day
-    },
-    checkBookingData() {
-      return new Promise((resolve, reject) => {
-        const { name, email, reservation } = this.booking
-        const REGEX_NUMBER = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-
-        /* ------------------ NAME ----------------- */
-        if (name === '') reject(new Error('Le nom est invalide'))
-
-        /* ------------------ EMAIL ----------------- */
-        if (!REGEX_NUMBER.test(email)) reject(new Error("L'email est invalide"))
-
-        /* ------------------ RESERVATION DAY ----------------- */
-        if (reservation.day === '') reject(new Error('Jour de réservation vide'))
-
-        resolve()
-      })
-    },
-    async goToStepPayment() {
-      const { id } = this.restaurant_details
-
+    async goToStepPayment(booking) {
       try {
-        await this.checkBookingData()
+        // const { id } = this.restaurant_details
 
-        await this.addToBookings({ idRestaurant: id, booking: this.booking })
+        //  await this.addToBookings({ idRestaurant: id, booking: this.booking })
+
+        console.log(booking)
 
         this.notyf.success('Réservation ajoutée')
 
-        this.booking = {
-          name: '',
-          email: '',
-          nb_of_persons: '1',
-          reservation: {
-            day: '',
-            time: '20:30',
-          },
-          message: '',
-          is_checkout: false,
-        }
+        this.isStepPayment = true
       } catch (error) {
         this.notyf.error(error.message)
       }
@@ -169,6 +83,7 @@ export default {
 <style scoped>
 .card-material {
   width: 500px;
+  overflow: hidden;
 }
 
 .card-material .card-material-content {
@@ -254,36 +169,7 @@ export default {
   margin: 30px 0;
 }
 
-.card-material .card-material-content .card-material-body .form {
-  margin: 20px 20px;
-}
-
-.card-material .card-material-content .card-material-body .form h3 {
-  margin-bottom: 10px;
-}
-
-.card-material .card-material-content .card-material-body .form input {
-  border-radius: 10px;
-  height: 20px;
-  width: 190px;
-  box-shadow: rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset, rgba(0, 0, 0, 0.9) 0px 0px 0px 1px;
-  font-size: 1em;
-}
-
-.card-material .card-material-content .card-material-body .form select {
-  height: 26px;
-  width: 190px;
-  border-radius: 10px;
-  box-shadow: rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset, rgba(0, 0, 0, 0.9) 0px 0px 0px 1px;
-  cursor: pointer;
-  text-align-last: center;
-}
-
-.card-material .card-material-content .card-material-body .bottom {
-  margin-top: 25px;
-}
-
-.active {
+.card-material .card-material-content .active {
   background-color: var(--primary-color) !important;
 }
 
@@ -291,10 +177,6 @@ export default {
   .card-material {
     margin-top: 20px;
     width: 450px;
-  }
-
-  .card-material .card-material-content .card-material-body .content-form {
-    flex-direction: column;
   }
 }
 </style>
---
+++ src/components/restaurantDetails/yfRestaurantDetailsBookingInfos.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="d-flex flex-column align-items-center card-material-body-content">
+    <div class="top">
+      <div class="d-flex flex-column align-items-center container-top">
+        <div class="d-flex content-form">
+          <div class="d-flex flex-column form">
+            <h3>Nom</h3>
+            <input type="text" v-model="booking.name" />
+          </div>
+          <div class="d-flex flex-column form">
+            <h3>E-mail</h3>
+            <input type="email" v-model="booking.email" />
+          </div>
+        </div>
+        <div class="d-flex content-form">
+          <div class="d-flex flex-column form">
+            <h3>Nombre de personne</h3>
+            <select v-model="booking.nb_of_persons">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+            </select>
+          </div>
+          <div class="d-flex flex-column form">
+            <h3>Heure de réservation</h3>
+            <select v-model="booking.reservation.time">
+              <option>20:30</option>
+              <option>21:00</option>
+              <option>21:30</option>
+              <option>22:00</option>
+              <option>22:30</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="middle">
+      <div class="d-flex align-items-center content-form">
+        <div class="d-flex flex-column form">
+          <h3>Jour de réservation</h3>
+          <yfDatePicker @dateSelected="handleReservationDay" />
+        </div>
+        <div class="d-flex flex-column form">
+          <h3>Message (falcutatif)</h3>
+          <textarea cols="24" rows="6" v-model="booking.message"></textarea>
+        </div>
+      </div>
+    </div>
+    <div class="bottom">
+      <button class="btn btn-secondary" @click="handleBooking">Suivant</button>
+    </div>
+  </div>
+</template>
+
+<script>
+/** Import */
+import yfDatePicker from '@/components/yfDatePicker.vue'
+
+export default {
+  name: 'yfRestaurantDetailsBookingInfos',
+  inject: ['notyf'],
+  components: {
+    yfDatePicker,
+  },
+  data() {
+    return {
+      booking: {
+        name: '',
+        email: '',
+        nb_of_persons: '1',
+        reservation: {
+          day: '',
+          time: '20:30',
+        },
+        message: '',
+        is_checkout: false,
+      },
+    }
+  },
+  methods: {
+    /**
+     * @function handleReservationDay
+     * @desc Set reservation day to booking
+     */
+    handleReservationDay(day) {
+      this.booking.reservation.day = day
+    },
+    /**
+     * @function checkBookingData
+     * @desc Check data of booking
+     */
+    checkBookingData() {
+      return new Promise((resolve, reject) => {
+        const { name, email, reservation } = this.booking
+        const REGEX_NUMBER = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+        /* ------------------ NAME ----------------- */
+        if (name === '') reject(new Error('Le nom est invalide'))
+
+        /* ------------------ EMAIL ----------------- */
+        if (!REGEX_NUMBER.test(email)) reject(new Error("L'email est invalide"))
+
+        /* ------------------ RESERVATION DAY ----------------- */
+        if (reservation.day === '') reject(new Error('Jour de réservation vide'))
+
+        resolve()
+      })
+    },
+    /**
+     * @function checkBookingData
+     * @desc handle data booking
+     */
+    async handleBooking() {
+      try {
+        // await this.checkBookingData()
+        this.$emit('bookingData', this.booking)
+      } catch (error) {
+        this.notyf.error(error.message)
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.card-material-body-content .form {
+  margin: 20px 20px;
+}
+
+.card-material .card-material-content .card-material-body-content .form h3 {
+  margin-bottom: 10px;
+}
+
+.card-material .card-material-content .card-material-body-content .form input {
+  border-radius: 10px;
+  height: 20px;
+  width: 190px;
+  box-shadow: rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset, rgba(0, 0, 0, 0.9) 0px 0px 0px 1px;
+  font-size: 1em;
+}
+
+.card-material .card-material-content .card-material-body-content .form select {
+  height: 26px;
+  width: 200px;
+  border-radius: 10px;
+  box-shadow: rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset, rgba(0, 0, 0, 0.9) 0px 0px 0px 1px;
+  cursor: pointer;
+  text-align-last: center;
+}
+
+.card-material .card-material-content .card-material-body-content .bottom {
+  margin-top: 25px;
+}
+
+@media screen and (max-width: 1280px) {
+  .card-material .card-material-content .card-material-body-content .content-form {
+    flex-direction: column;
+  }
+}
+</style>
