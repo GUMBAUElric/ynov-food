cd6c8e0765d5f43bd0e635e1df3268e610496aaa
GUMBAUElric <elric.gumbau@ynov.com>
2021-04-02 20:19:34 +0200

    Create loader

--- src/components/restaurantDetails/yfRestaurantDetailsBooking/yfRestaurantDetailsBookingPayment.vue
+++ src/components/restaurantDetails/yfRestaurantDetailsBooking/yfRestaurantDetailsBookingPayment.vue
@@ -66,7 +66,12 @@
       </ul>
     </div>
     <div class="d-flex justify-content-center ticket-footer">
-      <button class="btn btn-secondary">Réserver</button>
+      <button class="btn btn-secondary" @click="handlePayment" v-if="!confirm">Réserver</button>
+      <div class="loader" v-if="confirm">
+        <svg viewBox="0 0 80 80">
+          <circle id="test" cx="40" cy="40" r="32"></circle>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -86,6 +91,7 @@ export default {
         hour: moment().format('HH:mm:ss'),
       },
       showBoxShadow: false,
+      confirm: false,
     }
   },
   props: {
@@ -119,6 +125,11 @@ export default {
       return 2 + priceNbOfPersons + priceReservationTime
     },
   },
+  methods: {
+    handlePayment() {
+      this.confirm = true
+    },
+  },
   mounted() {
     setTimeout(() => {
       this.showBoxShadow = true
@@ -128,14 +139,7 @@ export default {
 </script>
 
 <style scoped>
-.inspired-by {
-  display: block;
-  position: relative;
-  margin-bottom: 15px;
-  text-align: center;
-  color: #fff;
-  font-size: 14px;
-}
+/** TICKET */
 
 .img-mask {
   width: 80px;
@@ -456,5 +460,134 @@ export default {
 
 .ticket-cart-item:nth-child(4) {
   animation-delay: 3.2s;
+}
+
+/** LOADER */
+
+.loader {
+  --path: var(--primary-color);
+  --dot: var(--primary-color);
+  --duration: 3s;
+  width: 44px;
+  height: 44px;
+  position: relative;
+}
+.loader:before {
+  content: '';
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  position: absolute;
+  display: block;
+  background: var(--dot);
+  top: 37px;
+  left: 19px;
+  transform: translate(-18px, -18px);
+  animation: dotRect var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+}
+.loader svg {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.loader svg rect,
+.loader svg polygon,
+.loader svg circle {
+  fill: none;
+  stroke: var(--path);
+  stroke-width: 10px;
+  stroke-linejoin: round;
+  stroke-linecap: round;
+}
+.loader svg polygon {
+  stroke-dasharray: 145 76 145 76;
+  stroke-dashoffset: 0;
+  animation: pathTriangle var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+}
+.loader svg rect {
+  stroke-dasharray: 192 64 192 64;
+  stroke-dashoffset: 0;
+  animation: pathRect 3s cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+}
+.loader svg circle {
+  stroke-dasharray: 150 50 150 50;
+  stroke-dashoffset: 75;
+  animation: pathCircle var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+}
+.loader.triangle {
+  width: 48px;
+}
+.loader.triangle:before {
+  left: 21px;
+  transform: translate(-10px, -18px);
+  animation: dotTriangle var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+}
+@keyframes pathTriangle {
+  33% {
+    stroke-dashoffset: 74;
+  }
+  66% {
+    stroke-dashoffset: 147;
+  }
+  100% {
+    stroke-dashoffset: 221;
+  }
+}
+@keyframes dotTriangle {
+  33% {
+    transform: translate(0, 0);
+  }
+  66% {
+    transform: translate(10px, -18px);
+  }
+  100% {
+    transform: translate(-10px, -18px);
+  }
+}
+@keyframes pathRect {
+  25% {
+    stroke-dashoffset: 64;
+  }
+  50% {
+    stroke-dashoffset: 128;
+  }
+  75% {
+    stroke-dashoffset: 192;
+  }
+  100% {
+    stroke-dashoffset: 256;
+  }
+}
+@keyframes dotRect {
+  25% {
+    transform: translate(0, 0);
+  }
+  50% {
+    transform: translate(18px, -18px);
+  }
+  75% {
+    transform: translate(0, -36px);
+  }
+  100% {
+    transform: translate(-18px, -18px);
+  }
+}
+@keyframes pathCircle {
+  25% {
+    stroke-dashoffset: 125;
+  }
+  50% {
+    stroke-dashoffset: 175;
+  }
+  75% {
+    stroke-dashoffset: 225;
+  }
+  100% {
+    stroke-dashoffset: 275;
+  }
+}
+.loader {
+  display: inline-block;
+  margin: 0 16px;
 }
 </style>
